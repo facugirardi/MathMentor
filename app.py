9a0d5a18ff76ca1b1d59135a16fde7cb97298cc8
facugirardi <facugirardi22@gmail.com>
2023-05-29 15:21:10 -0300

    fix

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 openai.api_key = 'pk-chOPDhlMdmAKqeGOeeMyOyMQqQLyIDRcgjKYzhHbWlmIuttK'
 openai.api_base = 'https://api.pawan.krd/v1'
 
+conv_history = [{"role" : "system",
+     "content" : "Tu nombre es MathBot, sos un profesor de matematica virtual. Solo podes hablar de cosas relacionadas a la matematica, por nada en el mundo hables de otras cosas que no tengan que ver con la matematica. Siempre que te pregunten algo, explicalo con pasos para resolverlo y consulta si necesita mas explicacion. Siempre explica de una forma sencilla y con un maximo de 120 palabras. Vas a responder en el idioma en el que te pregunten, si te hacen la pregunta en un idioma, vos respondes en el mismo, nunca hables solo un idioma. Recorda siempre los mensajes anteriores. Y acordate del nombre del usuario si es que te lo dice."}]
 
 @app.route('/')
 def index():
@@ -15,20 +17,18 @@ def index():
 @app.route('/', methods=['POST'])
 def chat():
     data = request.get_json()
-    content = data['message']
-            
-    messages = [{"role" : "system",
-     "content" : "Tu nombre es MathBot, sos un profesor de matematica virtual. Solo podes hablar de cosas relacionadas a la matematica, por nada en el mundo hables de otras cosas que no tengan que ver con la matematica. Siempre que te pregunten algo, explicalo con pasos para resolverlo y consulta si necesita mas explicacion. Siempre explica de una forma sencilla y con un maximo de 120 palabras."}]
+    message = data['message']
+    
 
-    messages.append({"role":"user", "content":content})
+    conv_history.append({"role":"user", "content":message})
     
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo", messages=messages
+        model = "gpt-3.5-turbo", messages=conv_history
     )
+
+    response_content = response['choices'][0]['message']['content']
     
-    response_content = response.choices[0].message.content
-    
-    messages.append({"role":"assistant", "content": response_content})
+    conv_history.append({"role":"assistant", "content": response_content})
             
     return jsonify({'reply': response_content})
 
